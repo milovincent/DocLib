7abf070369a23b1552eca24083fb19596092e38f
Doctor Number Four <miloszecket@gmail.com>
2019-05-07 08:30:21 -0400

    sample -ap

--- sampleBot.py
+++ sampleBot.py
@@ -3,8 +3,8 @@ from socketBot import bot
 def sample(msg):
     return "this is a sample message"
 
-sampleBot = bot("sample bot for AP", "xkcd")
-sampleBot.regexes = {"^!ap$":sample}
+sampleBot = bot("sample bot", "test")
+sampleBot.regexes = {"^!test$":sample}
 
 
 
