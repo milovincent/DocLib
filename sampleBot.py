0d8bf4c6e4176334d668b7d0b9267f03f2095ad5
Doctor Number Four <miloszecket@gmail.com>
2019-05-10 11:52:17 -0400

    rename

--- sampleBot.py
+++ sampleBot.py
@@ -1,10 +1,16 @@
-from socketBot import bot
+from doclib import bot
 
 def sample(msg):
     return "this is a sample message"
 
+def evaluate(msg):
+    try:
+        return eval(re.search("^!calc (.*)$",msg['data']['content']).group(1))
+    except:
+        return "nice try"
+
 sampleBot = bot("sample bot", "test")
-sampleBot.regexes = {"^!test$":sample}
+sampleBot.regexes = {"^!test$":sample, "^!calc (.*)$":evaluate}
 
 
 
